52ddd09737e30d66557ffc61181c859f4205efe6
Madso02 <mads.s.olesen@gmail.com>
2024-10-28 17:59:15 +0100

    changes

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,15 +1,31 @@
-<!-- YOU CAN DELETE EVERYTHING IN THIS PAGE -->
+<script lang="ts">
+	import type { PageData } from './$types';
 
-<div class="container h-full mx-auto flex justify-center items-center">
-	<div class="space-y-5">
-		<h1 class="h1">Let's get cracking bones!</h1>
-		<p>Start by exploring:</p>
-		<ul>
-			<li><code class="code">/src/routes/+layout.svelte</code> - barebones layout</li>
-			<li><code class="code">/src/app.postcss</code> - app wide css</li>
-			<li>
-				<code class="code">/src/routes/+page.svelte</code> - this page, you can replace the contents
-			</li>
-		</ul>
-	</div>
+	let { data }: { data: PageData } = $props();
+</script>
+
+<div class="flex justify-center gap-6 flex-wrap">
+	{#each data.overviews as overview}
+		<a href="/overview/{overview.id}" class="block card w-80 p-4">
+			<header class="card-header">
+				<p class="h1">{overview.name}</p>
+				<p>{overview.description}</p>
+				<br>
+			</header>
+			<section class="p-4 flex gap-3 flex-wrap">
+				<span class="chip variant-filled-secondary font-bold relative">
+					AAPL
+					<span class="absolute -top-[5px] -right-[5px] z-10 fi fi-us"></span>
+				</span>
+				<span class="chip variant-filled-secondary font-bold relative">
+					AAPL
+					<span class="absolute -top-[5px] -right-[5px] z-10 fi fi-us"></span>
+				</span>
+				<span class="chip variant-filled-secondary font-bold relative">
+					AAPL
+					<span class="absolute -top-[5px] -right-[5px] z-10 fi fi-us"></span>
+				</span>
+			</section>
+		</a>
+	{/each}
 </div>
---
+++ src/routes/overview/[id]/+page.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+	import type { PageData } from '../$types';
+
+	let { data }: { data: PageData } = $props();
+	let overview = data.overview;
+</script>
+
+<div class="bg-surface-500 p-8 rounded-lg">
+	<h1 class="h1">{overview.name}</h1>
+	<p>{overview.description}</p>
+</div>
